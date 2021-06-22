5ab91af29fca03db12456b91dd176f51a67a3878
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-21 22:17:59 -0700

    Cleaned up entry file.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ const path = require("path");
 const cwd = process.cwd();
 
 const Dostman = require("./dostman");
-const Request = require("./request");
-const { readFile, writeFile } = require("./utils");
 
 // For dev purposes
 async function main() {
   const filePath = path.join(cwd, "src", "sample.dostman");
   const dostman = new Dostman(filePath);
   console.log(dostman);
-  //   const chunk = readFile(filePath);
-
-  //   const request = new Request(chunk);
-  //   console.log(request);
-  //   await request.execute();
-  //   writeFile("./output.json", request.toString());
-
-  //   console.log(request.toJson());
 }
 
 main();
