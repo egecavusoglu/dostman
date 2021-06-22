57a20534acbfd536edfc00d7ecb45ce02f21e9cb
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-21 17:41:42 -0700

    Created Regex factory to parse decorators.

--- src/Chunk.js
+++ src/Chunk.js
@@ -8,9 +8,9 @@ class Chunk {
   }
 
   parseChunk(chunk) {
-    chunk = chunk.replace(/(\r\n|\n|\r)/gm, " ");
-    chunk = chunk.replace(/\s\s+/g, " ");
-    chunk += "@";
+    chunk = chunk.replace(/(\r\n|\n|\r)/gm, " "); // Remove all line breaks
+    chunk = chunk.replace(/\s\s+/g, " "); // Trim all whitespace to 1 space.
+    chunk += "@"; // Add @ at the end for regex to work correctly.
     return {
       method: this.parseMethod(chunk),
       url: this.parseUrl(chunk),
@@ -20,34 +20,37 @@ class Chunk {
   }
 
   parseMethod(chunk) {
-    let method = chunk.match(/(@method)(.*?)(?=@)/g)[0].replace("@method ", "");
+    let method = this.parseDecorator("method", chunk);
     method = this.trimString(method);
     return method;
   }
 
   parseUrl(chunk) {
-    let url = chunk.match(/(@url)(.*?)(?=@)/g)[0].replace("@url ", "");
+    let url = this.parseDecorator("url", chunk);
     url = this.trimString(url);
     return url;
   }
 
   parseHeaders(chunk) {
-    let headers = chunk
-      .match(/(@headers)(.*?)(?=@)/g)[0]
-      .replace("@headers ", "");
+    let headers = this.parseDecorator("headers", chunk);
     headers = headers.split(","); // Seperate into array with ","
     headers.forEach((h, ind) => (headers[ind] = this.trimString(h)));
     return headers;
   }
 
   parseBody(chunk) {
-    let body = chunk.match(/(@body)(.*?)(?=@)/g)[0].replace("@body ", "");
+    let body = this.parseDecorator("body", chunk);
     body = JSON.parse(body);
     return body;
   }
 
   trimString(str) {
     return str.replace(/\s+/g, "");
+  }
+
+  parseDecorator(decorator, chunk) {
+    const regex = new RegExp(`(@${decorator})(.*?)(?=@)`);
+    return chunk.match(regex)[0].replace(`@${decorator} `, "");
   }
 }
 
