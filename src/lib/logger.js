bd125dea88b5bfd9ca7df7dfa3ca09452a78cbd0
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-06-29 19:45:30 -0700

    Added config to winston logger.

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,16 +1,33 @@
+/**
+ * Global logger for the cli.
+ * Use as follows:
+ * logger.info("message");
+ * logger.warn("message");
+ * logger.error("message");
+ */
+
 const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const symbols = {
+    info: '✅',
+    warn: '⚠️ ',
+    error: '❌',
+};
+
+const myFormat = format((info, opts) => {
+    // const level = info.level;
+    // console.log(typeof level);
+    info.level = symbols[info.level];
+    return info;
+});
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    format: combine(myFormat()),
     transports: [
-        //
-        // - Write all logs with level `error` and below to `error.log`
-        // - Write all logs with level `info` and below to `combined.log`
-        //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
+        // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        // new winston.transports.File({ filename: 'combined.log' }),
     ],
 });
 
